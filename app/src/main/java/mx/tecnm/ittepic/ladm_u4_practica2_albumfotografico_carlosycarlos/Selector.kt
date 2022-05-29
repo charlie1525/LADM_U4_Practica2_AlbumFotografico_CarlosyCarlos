eb22602d2d307b121e56b8e9433a7cb55a8eac0f
Carlos Uriel Fregoso Espericueta <caurfregosoes@ittepic.edu.mx>
2022-05-29 13:19:05 -0600

    Fisrt commit

---
+++ app/src/main/java/mx/tecnm/ittepic/ladm_u4_practica2_albumfotografico_carlosycarlos/Selector.kt
@@ -0,0 +1,11 @@
+package mx.tecnm.ittepic.ladm_u4_practica2_albumfotografico_carlosycarlos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Selector : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_selector)
+    }
+}
