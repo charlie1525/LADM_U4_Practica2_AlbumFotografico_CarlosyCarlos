6b6f4d9773a374df5af0ab535df2e4072149fc5d
Carlos Uriel Fregoso Espericueta <caurfregosoes@ittepic.edu.mx>
2022-05-30 21:34:07 -0600

    Adding the design for the login, also, added functionality to log out, about us, exit the app, and the register authentication with email and password

--- app/src/main/java/mx/tecnm/ittepic/ladm_u4_practica2_albumfotografico_carlosycarlos/MainActivity.kt
+++ app/src/main/java/mx/tecnm/ittepic/ladm_u4_practica2_albumfotografico_carlosycarlos/MainActivity.kt
@@ -1,11 +1,85 @@
 package mx.tecnm.ittepic.ladm_u4_practica2_albumfotografico_carlosycarlos
 
+import android.app.ProgressDialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.google.firebase.auth.FirebaseAuth
+import mx.tecnm.ittepic.ladm_u4_practica2_albumfotografico_carlosycarlos.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val autenticacion = FirebaseAuth.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val user = FirebaseAuth.getInstance().currentUser
+        if(user != null) invocaSeleccion()// fin de la checada si hay un usuario ya logeado
+
+        binding.inscribir.setOnClickListener {
+            autenticacion.createUserWithEmailAndPassword(binding.correo.text.toString(), binding.txtPass.text.toString())
+                .addOnCompleteListener {
+                    if(it.isSuccessful){
+                        binding.correo.setText("")
+                        binding.txtPass.setText("")
+                        mensaje("Usuario Creado")
+                        autenticacion.signOut()
+                    }else{ alerta("Error de creacion de usuario") }
+                }
+        }
+
+        binding.autenticar.setOnClickListener {
+            val dialogo = ProgressDialog(this)
+            dialogo.setMessage("Se esta autenticando el correo")
+            dialogo.setCancelable(false)
+            dialogo.show()
+
+            autenticacion.signInWithEmailAndPassword(
+                binding.correo.text.toString(),
+                binding.txtPass.text.toString()
+            ).addOnCompleteListener {
+                dialogo.dismiss()
+                if(it.isSuccessful){
+                    invocaSeleccion()
+                    return@addOnCompleteListener
+                }else{
+                   alerta("Correo o contraseña erroneos")
+                }
+            }
+        }
+
+        binding.recuperar.setOnClickListener {
+            val dialogo = ProgressDialog(this)
+            dialogo.setCancelable(false)
+            dialogo.setMessage("Mandando correo de recuperación")
+            dialogo.show()
+
+            autenticacion.sendPasswordResetEmail(
+                binding.correo.text.toString()
+            ).addOnSuccessListener {
+                dialogo.dismiss()
+                alerta("Correo enviado, favor de revisar su bandeja de entrada")
+            }
+        }
+
+    }// fin del on create
+
+    private fun invocaSeleccion() {
+        val selectionWindow = Intent(this,Selector::class.java)
+        startActivity(selectionWindow)
+        finish()
     }
-}
+
+    private fun alerta(cadena:String){
+        AlertDialog.Builder(this).setTitle("Atencion!!")
+            .setMessage(cadena).show()
+    }
+
+    private fun mensaje(cadena:String){
+        Toast.makeText(this, cadena, Toast.LENGTH_SHORT).show()
+    }
+}// fin del main activity
--- app/src/main/java/mx/tecnm/ittepic/ladm_u4_practica2_albumfotografico_carlosycarlos/Selector.kt
+++ app/src/main/java/mx/tecnm/ittepic/ladm_u4_practica2_albumfotografico_carlosycarlos/Selector.kt
@@ -1,11 +1,49 @@
 package mx.tecnm.ittepic.ladm_u4_practica2_albumfotografico_carlosycarlos
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.google.firebase.auth.FirebaseAuth
 
 class Selector : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_selector)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu,menu)
+        return true
+    }// match del activity desde el XML
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.mnAboutM -> {
+                alerta("17401063 CARLOS EDUARDO ROBLES LOPEZ\n" +
+                        "17400978 CARLOS URIEL FREGOSO ESPERICUETA\n")
+            }
+            R.id.mnSessionM -> {
+                FirebaseAuth.getInstance().signOut()
+                startActivity(Intent(this,MainActivity::class.java))
+                finish()
+            }
+            R.id.mnExitM -> {
+                finish()
+            }
+        }
+        return true
+    }
+
+    private fun alerta(cadena:String){
+        AlertDialog.Builder(this).setTitle("Atencion!!")
+            .setMessage(cadena).show()
+    }
+
+    private fun mensaje(cadena:String){
+        Toast.makeText(this, cadena, Toast.LENGTH_SHORT).show()
     }
 }
